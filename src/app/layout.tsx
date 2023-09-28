076e3fe71608cce486c2724930766ae56bd68480
Lucas P1nheiro <lps1704@gmail.com>
2023-09-28 09:26:42 -0300

    change directories structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Space_Grotesk } from 'next/font/google'
-import Header from './components/Header'
+import Header from '../components/Header'
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import About from './components/About'
-import getMyAge from './utils/getMyAge'
+import About from '../components/About'
+import getMyAge from '../utils/getMyAge'
 
 export default function Home() {
   return (
     <main className="min-h-screen w-screen py-12">
       <div className="mx-auto flex h-screen w-3/5 flex-col items-start justify-center gap-8">
-        <h1 className="bg-gradient-to-r from-[#B993D6] to-[#8CA6DB] bg-clip-text text-5xl font-bold text-transparent">
+        <h1 className="bg-gradient-to-r from-[#8360c3] to-[#2ebf91] bg-clip-text text-5xl font-bold text-transparent">
           Hi, my name is Lucas Pinheiro
         </h1>
         <h3 className="text-2xl text-secondary">
