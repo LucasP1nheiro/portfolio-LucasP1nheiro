18b9f559a58770d96b67c0e59c7817c6cdb0ae8a
Lucas P1nheiro <lps1704@gmail.com>
2023-10-01 21:06:58 -0300

    add logo and favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import React from 'react'
 import NavItem from './NavItem'
+import Image from 'next/image'
+import logo from '../assets/logo.svg'
 
 const Header = () => {
   return (
@@ -9,7 +11,13 @@ const Header = () => {
         <ul className="mx-auto flex h-full w-3/5 items-center justify-between">
           <li>
             <Link href="/" className="text-primary">
-              Logo
+              <Image
+                src={logo.src}
+                alt="Logo Image"
+                priority
+                width={31}
+                height={41}
+              />
             </Link>
           </li>
           <div className="flex h-full items-center gap-4 py-5">
