4446d7007c7b2513d2d75df9a79a81bcb0f9f217
Lucas P1nheiro <lps1704@gmail.com>
2023-10-05 11:43:29 -0300

    update project page styling

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -23,7 +23,7 @@ const page = ({ params: { slug } }: PageProps) => {
     <main className="mx-auto flex min-h-screen w-full flex-col items-start justify-center space-y-12 px-4 py-36 md:w-3/5 md:px-0">
       <div className="flex w-full flex-col  justify-between gap-12 text-center lg:flex-row lg:items-center">
         <h1
-          className={`bg-gradient-to-r from-[#9488FE] to-[#88CFF2] bg-clip-text text-5xl font-bold text-transparent`}
+          className={`bg-gradient-to-r from-[#81FFE3] to-[#88FF9D] bg-clip-text text-5xl font-bold text-transparent`}
         >
           {project.title}
         </h1>
@@ -57,7 +57,7 @@ const page = ({ params: { slug } }: PageProps) => {
       />
 
       <section
-        className={`prose w-[300px] p-2 prose-h1:text-primary prose-h2:text-primary prose-h3:text-primary prose-p:text-secondary prose-a:text-[#88CFF2] prose-strong:text-[#88CFF2] prose-ol:text-secondary prose-ul:text-secondary md:w-full `}
+        className={`prose w-[300px] p-2 prose-h1:text-primary prose-h2:text-primary prose-h3:text-primary prose-p:text-secondary prose-a:text-[#88FF9D] prose-strong:text-[#88FF9D] prose-ol:text-secondary prose-ul:text-secondary md:w-full `}
       >
         {project?.markdown}
       </section>
