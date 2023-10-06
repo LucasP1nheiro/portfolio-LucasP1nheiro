3e1a072106a0291543f6714519b768c60bdf9760
Lucas P1nheiro <lps1704@gmail.com>
2023-10-06 11:12:39 -0300

    add animation on projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,23 +1,35 @@
+'use client'
 import ProjectsCard from '@/components/ProjectsCard'
 import React from 'react'
 import { projectsInfo } from '../../data/projects'
+import { motion as m } from 'framer-motion'
 
 const page = () => {
   return (
     <main className="flex min-h-screen w-screen flex-col items-center justify-start space-y-16 p-2 pt-36 text-center">
-      <h1 className="bg-gradient-to-r from-[#FD80C1] to-[#BF80E5] bg-clip-text text-5xl font-extrabold text-transparent">
+      <m.h1
+        initial={{ opacity: 0, y: 5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.1 }}
+        className="bg-gradient-to-r from-[#FD80C1] to-[#BF80E5] bg-clip-text text-5xl font-extrabold text-transparent"
+      >
         Projects
-      </h1>
+      </m.h1>
 
-      <p className="text-lg text-secondary">
+      <m.p
+        initial={{ opacity: 0, y: 5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.1, delay: 0.1 }}
+        className="text-lg text-secondary"
+      >
         Here you can find{' '}
         <span className="text-primary">{projectsInfo.length} projects</span>{' '}
         I&apos;ve selected to showcase my skills as a developer.
-      </p>
+      </m.p>
 
       <section className="grid w-5/6 grid-cols-1 gap-16 md:grid-cols-2 2xl:grid-cols-3">
-        {projectsInfo.map((project) => (
-          <ProjectsCard key={project.title} project={project} />
+        {projectsInfo.map((project, i) => (
+          <ProjectsCard key={project.title} project={project} index={i} />
         ))}
       </section>
     </main>
--- src/components/ProjectsCard.tsx
+++ src/components/ProjectsCard.tsx
@@ -1,45 +1,55 @@
+'use client'
+
 import { ProjectInfoProps } from '@/data/projects'
 import Link from 'next/link'
 import Image from 'next/image'
+import { motion as m } from 'framer-motion'
 
 interface ProjectsCardProps {
   project: ProjectInfoProps
+  index: number
 }
 
-const ProjectsCard = ({ project }: ProjectsCardProps) => {
+const ProjectsCard = ({ project, index }: ProjectsCardProps) => {
   return (
-    <Link
-      href={`/projects/${project.href.toLocaleLowerCase()}`}
-      className="group flex flex-col items-start justify-between gap-4 rounded-md bg-primary/10 p-4 text-start transition-all duration-300 hover:bg-primary/10 lg:bg-background"
+    <m.div
+      initial={{ opacity: 0, y: 5 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.1, delay: 0.1 * index }}
     >
-      <div className="space-y-5">
-        <Image
-          src={project.image}
-          priority
-          alt={`${project.title} project image`}
-          width={96}
-          height={96}
-          className="h-auto w-full grayscale-0 group-hover:grayscale-0 lg:grayscale"
-        />
-        <h1 className="text-2xl font-extrabold text-primary">
-          {project.title}
-        </h1>
-        <p className="text-secondary">{project.description}</p>
-      </div>
-      <div className="flex items-center gap-4">
-        <h3 className="text-lg text-primary">Stack : </h3>
-        {project.stack.map((skill) => (
+      <Link
+        href={`/projects/${project.href.toLocaleLowerCase()}`}
+        className="group flex flex-col items-start justify-between gap-4 rounded-md bg-primary/10 p-4 text-start transition-all duration-300 hover:bg-primary/10 lg:bg-background"
+      >
+        <div className="space-y-5">
           <Image
-            key={skill.name}
-            src={skill.image}
+            src={project.image}
             priority
-            alt={`${skill.name} project image`}
-            width={24}
-            height={24}
+            alt={`${project.title} project image`}
+            width={96}
+            height={96}
+            className="h-auto w-full grayscale-0 group-hover:grayscale-0 lg:grayscale"
           />
-        ))}
-      </div>
-    </Link>
+          <h1 className="text-2xl font-extrabold text-primary">
+            {project.title}
+          </h1>
+          <p className="text-secondary">{project.description}</p>
+        </div>
+        <div className="flex items-center gap-4">
+          <h3 className="text-lg text-primary">Stack : </h3>
+          {project.stack.map((skill) => (
+            <Image
+              key={skill.name}
+              src={skill.image}
+              priority
+              alt={`${skill.name} project image`}
+              width={24}
+              height={24}
+            />
+          ))}
+        </div>
+      </Link>
+    </m.div>
   )
 }
 
